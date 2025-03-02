4ddf40cd5840791381e1bc26af03152cea15bb1d
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-03-01 18:08:33 -0800

    Fix parser with updated ICS format

--- icsparser.py
+++ icsparser.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class ICSParser():
     '''
     Takes in ics file and outputs it into dictionary format
@@ -16,31 +18,34 @@ class ICSParser():
         text = text.split("BEGIN:VEVENT")
         event = {}
         for item in text:
+            print(item)
             if 'SUMMARY:' in item:
-                assignmentName = item.split("SUMMARY:")[1].split('URL:')[0]
+                assignmentName = item.split("SUMMARY:")[1].split('URL;')[0]
                 className, assignmentName = self.getSplitString(assignmentName, "[", " (")
-                date = self.cleanString(str(item.split("VALUE=DATE:")[1].split("CLASS:")[0]), "\r", "\n")
+                date = None
+                if 'DTSTART;' in item: # purely a date with default due date at 11:59pm, #DTSTART;VALUE=DATE;VALUE=DATE:20250322
+                    date = self.cleanString(str(item.split("VALUE=DATE:")[1].split("CLASS:")[0]), "\r", "\n")
+                elif 'DTSTART:' in item: # includes date and time, #DTSTART:20250317T183000Z\nDTEND:20250317T183000Z
+                    date = self.cleanString(str(item.split("DTSTART:")[1].split("DTEND:")[0]), "\r", "\n")
+
                 event[assignmentName] = {
                     "Date" : date,
                     "Class" : className
                 }
         return event
 
-    def cleanString(self, string, *args):
+    def cleanString(self, string: str, *args):
         for str in args:
             splitstring = string.split(str)
-            splitstring = [x.lstrip() for x in splitstring]
+            splitstring = [x.lstrip().rstrip() for x in splitstring]
             string = ''.join(splitstring)
         return string
 
-    def getSplitString(self, string, str1, str2):
-        idx1 = string.find(str1)
-        idx2 = string.find(str2)
+    def getSplitString(self, string: str, str1: str, str2: str):
+        idx1 = string.rfind(str1)
+        idx2 = string.rfind(str2)
 
         splitStr1 = self.cleanString(string[idx1 + len(str1): idx2], "\r", "\n")
-        splitStr2 = self.cleanString(string[0:idx1-1], " [", "(")
+        splitStr2 = self.cleanString(string[0:idx1-1], "\r", "\n", "\\")
 
         return splitStr1, splitStr2
-
-        
-            
